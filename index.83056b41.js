bca397539c4645cd23ceccdf8435c4bf6e65abe0
voidmain69 <photoreznichenko@gmail.com>
2024-09-09 00:51:29 +0300

    make build

---
+++ index.83056b41.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.querySelector("#largeImg"),t=document.querySelector("#thumbs"),n=function(t){e.setAttribute("src",t)};t.addEventListener("click",function(e){e.preventDefault();var t=e.target.closest("a");t&&n(t.getAttribute("href"))})});
+//# sourceMappingURL=index.83056b41.js.map
